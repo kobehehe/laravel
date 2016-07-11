ff7e9f03fdda08dd00434ca91fb0c45247a72124
xuxiaochen <xuxiaochen@innobuddy.com>
2016-07-11 15:10:21 +0800

    增加增加文章方法

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -13,4 +13,29 @@ class ArticleController extends Controller
 	{
     	return view('admin/article/index')->withArticles(Article::all());
 	}
+
+	public function create()  
+	{
+    	return view('admin/article/create');
+	}
+
+	public function store(Request $request)  
+	{
+	    $this->validate($request, [
+	        'title' => 'required|unique:articles|max:255',
+	        'body' => 'required',
+	    ]);
+
+	    $article = new Article;
+	    $article->title = $request->get('title');
+	    $article->body = $request->get('body');
+	    $article->user_id = $request->user()->id;
+
+	    if ($article->save()) {
+	        return redirect('admin/article');
+	    } else {
+	        return redirect()->back()->withInput()->withErrors('保存失败！');
+	    }
+	}
+
 }
