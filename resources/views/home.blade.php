f566b4b29196b01284736eeac1af6bf7668f6b2a
xuxiaochen <xuxiaochen@innobuddy.com>
2016-08-09 17:43:38 +0800

    增加首页

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,183 @@
-<!DOCTYPE html>  
-<html lang="en">  
-<head>  
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Learn Laravel 5</title>
-
-    <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Football Bootstarp Website Template | Home :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Passion+One:400,700,900' rel='stylesheet' type='text/css'>
+<!--//fonts-->
+ <script src="js/responsiveslides.min.js"></script>
+ <script>
+    // You can also use "$(window).load(function() {"
+    $(function () {
+      $("#slider2").responsiveSlides({
+        auto: true,
+        pager: true,
+        speed: 300,
+        namespace: "callbacks",
+      });
+    });
+  </script>
 </head>
-
-    <div id="title" style="text-align: center;">
-        <h1>Learn Laravel 5</h1>
-        <div style="padding: 5px; font-size: 16px;">Learn Laravel 5</div>
-    </div>
-    <hr>
-    <div id="content">
-        <ul>
-            @foreach ($articles as $article)
-            <li style="margin: 50px 0;">
-                <div class="title">
-                    <a href="{{ url('article/'.$article->id) }}">
-                        <h4>{{ $article->title }}</h4>
-                    </a>
-                </div>
-                <div class="body">
-                    <p>{{ $article->body }}</p>
-                </div>
-            </li>
-            @endforeach
-        </ul>
-    </div>
-
-</body>  
-</html>  
+<body> 
+<!--header-->
+	<div class="header">
+		<div class="header-top">
+			<div class="container">
+			<p class="header-para">Free Shipping on orders over $50 USD</p>
+			
+				<ul class="sign">
+					<li ><a href="signin.html" >SIGN IN</a></li>
+					<li><a href="#" ><span > </span></a></li>			
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+	</div>
+	<div class="header-bottom-top">
+		<div class="container">
+			<div class="search">	
+				<input type="text" value="Search website" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}" >
+				<input type="submit"  value="">	
+			</div>
+			<div class="clearfix"> </div>
+			<div class="header-bottom">			
+				<div class="logo">
+					<a href="index.html"><img src="images/log.png" alt=" " /></a>
+				</div>
+				<div class="top-nav">
+					<span class="menu"> </span>
+					<ul>
+						<li class="active" ><a href="index.html" class="scroll">Home</a></li>
+						<li><a href="about.html" class="scroll"> About</a></li>
+						<li><a href="teams.html" class="scroll">Team</a></li>
+						
+						<li><a href="services.html" class="scroll">Services</a></li>
+						<li><a href="sports.html" class="scroll">Sports </a></li>
+						<li><a href="contact.html" class="scroll">Contact</a></li>
+					</ul>
+					<!--script-->
+				<script>
+					$("span.menu").click(function(){
+						$(".top-nav ul").slideToggle(500, function(){
+						});
+					});
+			</script>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		</div>
+	</div>
+		<!--banner-->
+		 <div class="slider">
+			<ul class="rslides" id="slider2">
+			  <li><a href="#"><img src="images/ban.jpg" alt="">
+			  <div class="banner">
+			  <span class=" matter "> </span>
+			  </div>
+			  </a></li>
+			  <li><a href="#"><img src="images/bb.jpg" alt="">
+			   <div class="banner">
+			  <span class=" matter"> </span>
+			  </div>
+			   </a></li>
+			  <li><a href="#"><img src="images/ban.jpg" alt="">
+				<div class="banner">
+			  <span class="matter "> </span>
+			  </div>
+			  </a></li>
+			   
+			  </a></li>
+			</ul>
+		</div>
+		
+		<!--content-->
+			<div class="content">
+				<div class="content-app">
+					<div class="container">
+						<h3>New from Football</h3>
+						<div class="content-from">
+							<div class="col-md-4 from-grid ">
+								<img class="img-responsive left-pic" src="images/pic.jpg" alt=" " />
+								<p>2015 fastpitch cf 7 lineup released</p>								
+							</div>
+							<div class="col-md-4 from-grid ">
+								<img class="img-responsive left-pic" src="images/pic1.jpg" alt=" " />
+								<p>2015 fastpitch cf 7 lineup released</p>								
+							</div>
+							<div class="col-md-4 from-grid ">
+								<img class="img-responsive left-pic" src="images/pic.jpg" alt=" " />
+								<p>2015 fastpitch cf 7 lineup released</p>								
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<!---->
+		<div class="content-app-up">
+			<div class="container">
+			<span class="line"> </span>
+			<h3>Trending from Social Media</h3>
+			<div class="regard">
+				<div class="regard-in">					
+					<p><a href="#"> about a day ago we replied to </a></p>
+					<p>@danielcook1996 Hi Daniel, we are sorry that we can't get something out to you sooner. If you have any questions regarding...</p>	
+					<a href="#"><span> </span></a>
+				</div>
+				<div class="regard-in">
+				<a href="#"><span class="camera"> </span></a>
+					<p><a href="#"> about a day ago we replied to </a></p>
+					<img class="img-responsive ago" src="images/pe.jpg" alt="">
+					<p class="col-d">@Football22 Chris Larsen with the #CL22. #SlowpitchSunday @FootballSP #beast</p>	
+				<div class="clearfix"> </div>
+				
+				</div>
+				<div class="regard-in">
+					<p><a href="#"> about a day ago we replied to </a></p>
+					<p>@danielcook1996 Hi Daniel, we are sorry that we can't get something out to you sooner. If you have any questions regarding...</p>	
+					<a href="#"><span class="face"> </span></a>
+				</div>
+				
+				
+			</div>
+			</div>		
+		</div>
+		<!---->		
+	</div>
+	<!--footer-->
+	<div class="footer">
+		<div class="container">
+		
+			<div class="foter-bottom">			
+				 <p class="footer-grid">Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+				<div class=" nav-top">				
+					<ul>
+						<li class="active" ><a href="index.html" class="scroll">Home</a></li>
+						<li><a href="about.html" class="scroll"> About</a></li>
+						<li><a href="teams.html" class="scroll">Team</a></li>					
+						<li><a href="services.html" class="scroll">Services</a></li>
+						<li><a href="sports.html" class="scroll">Sports </a></li>
+						<li><a href="contact.html" class="scroll">Contact</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+			</div>
+		</body>
+		</html>
